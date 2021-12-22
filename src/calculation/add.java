16c34b8fc395e77e02b60ed4ca2c2cc7854f4062
noufalmc123 <47408615+noufalmc123@users.noreply.github.com>
2021-12-22 21:35:55 +0530

    latest

---
+++ src/calculation/add.java
@@ -0,0 +1,13 @@
+package calculation;
+
+public class add {
+  public static void main(String[] arg)
+  { 
+	  System.out.println("Enter Twp Numbers");
+	  int a=10;
+	  int b=10;
+	  int c=a+b;
+	  int d=a/b;
+	  System.out.println("Sum is "+c+"Div is"+d);
+  }
+}
